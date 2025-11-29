839792e86642a557e6fba367cd27f01db0eea2fd
Sakshi Singh <sakshisingh44o44@gmail.com>
2025-11-29 18:01:02 +0530

    Add files via upload

---
+++ circumference of circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int r,circumference;
+	printf("enter the values");
+	scanf("%d", &r);
+	circumference = 2 * 3.14 * r;
+	printf("%d",circumference);
+	return 0;
+}
